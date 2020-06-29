72b944625dc340ff829ad2787668c6299be4ae54
goldthorp <simon@goldthorp.com>
2020-06-29 06:24:32 -0400

    CHIN-8

--- app/src/main/java/com/goldthorp/ching/data/EntryDao.java
+++ app/src/main/java/com/goldthorp/ching/data/EntryDao.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Dao
 public interface EntryDao {
-  @Query("SELECT * FROM entry")
+  @Query("SELECT * FROM entry ORDER BY timestamp DESC")
   LiveData<List<Entry>> getAll();
 
   @Insert
